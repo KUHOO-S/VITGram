06cd2d340ce69762f3e497b1624fe95975657169
KUHOO-S <kuhoo1999@rediffmail.com>
2020-05-31 22:25:25 +0530

    path corrected

--- registerPage.php
+++ registerPage.php
@@ -12,13 +12,13 @@
 <body>
 <h1>Register</h1>
 
-<form class="myForm" method="POST" action="register.php">
+<form class="myForm" method="POST" action="../public/scripts/register.php">
     <input type="text" name="fname" id="fname" placeholder="Enter First Name">
     <input type="text" name="lname" id="lname" placeholder="Enter Last Name">
     <input type="email" name="email" id="email" placeholder="xyz@abc.com">
     <input type="text" name="username" id="username" placeholder="Enter UserName">
-    <input type="pass" name="pass" id="pass" placeholder="Enter Password">
-    <input type="cpass" name="cpass" id="cpass" placeholder="Confirm Password">
+    <input type="password" name="pass" id="pass" placeholder="Enter Password">
+    <input type="password" name="cpass" id="cpass" placeholder="Confirm Password">
     <input type="submit" name="register">
 </form>
 
